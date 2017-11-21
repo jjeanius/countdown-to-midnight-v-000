2511b16ab90747695c1510de9b4b6d6b96815e0d
Judy McGoldrick <jjeanius@verizon.net>
2017-11-21 16:08:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
-def countdown
+def countdown("HAPPY NEW YEAR")
   countdown = -10
   while countdown > 0
     puts "#{countdown} SECOND"
     count -= 1
      sleep(5)
-    return "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
   end
 end
